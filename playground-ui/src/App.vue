3ad7013c5308b967b992832090ada80df5fe621a
leishanglin <reasonly8@sina.com>
2024-12-09 17:46:16 +0800

    feat: daily updates

--- playground-ui/src/App.vue
+++ playground-ui/src/App.vue
@@ -1,34 +1,43 @@
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="content-title">
-    早安， 
-    <span class="text-red">Serati Ma</span>
-    <span class="welcome-text">，祝你开心每一天！</span>
+  <div class="button-container">
+    <a href="https://bailian.console.aliyun.com/" target="_blank" rel="nofollow" class="manage-console">管理控制台</a>
+    <a href="https://page.aliyun.com/form/act733831898/index.htm" target="_blank" class="consult-now">立即咨询</a>
   </div>
 </template>
 
 <style lang="less" scoped>
-.content-title {
-  border: none;
-  color: rgba(0, 0, 0, 0.88);
-  font-size: 20px;
-  font-weight: 500;
-  line-height: 28px;
-  list-style: none;
-  margin: 0 0 12px;
-  text-decoration: none;
-  width: 922px; // 四舍五入取整
-  height: 28px;
-  display: block;
+.button-container {
+  height: 44px;
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
 
-  .text-red {
-    color: rgb(248, 113, 113);
+  .manage-console {
+    background-color: rgb(255, 106, 0);
+    border: 1px solid transparent;
+    color: rgb(255, 255, 255);
+    font-weight: 700;
+    line-height: 42px;
+    text-align: center;
+    text-decoration: none;
+    width: 160px;
+    height: 44px;
+    display: block;
   }
 
-  .welcome-text {
-    color: rgba(0, 0, 0, 0.88);
+  .consult-now {
+    border: 1px solid rgb(156, 164, 175);
+    color: rgb(61, 73, 92);
+    font-weight: 700;
+    line-height: 42px;
+    margin-left: 16px;
+    text-align: center;
+    text-decoration: none;
+    width: 160px;
+    height: 44px;
+    display: block;
   }
 }
 </style>
