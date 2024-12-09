c09508d6f9cb43c6ee5274ffa5bac530ca5d4aa0
leishanglin <reasonly8@sina.com>
2024-12-09 09:15:25 +0800

    feat: add playground

---
+++ playground/src/App.vue
@@ -0,0 +1,155 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="ant-card-body"
+    style="
+      block-size: 133.711px;
+      border-block-color: rgba(0, 0, 0, 0.88);
+      border-color: rgba(0, 0, 0, 0.88);
+      border-bottom-left-radius: 8px;
+      border-bottom-right-radius: 8px;
+      border-end-end-radius: 8px;
+      border-end-start-radius: 8px;
+      border-inline-color: rgba(0, 0, 0, 0.88);
+      caret-color: rgba(0, 0, 0, 0.88);
+      color: rgba(0, 0, 0, 0.88);
+      column-rule-color: rgba(0, 0, 0, 0.88);
+      font-family: -apple-system, 'system-ui', 'Segoe UI', Roboto,
+        'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
+        'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+      font-size: 14px;
+      height: 133.711px;
+      inline-size: 1437px;
+      line-height: 22px;
+      list-style-type: none;
+      outline-color: rgba(0, 0, 0, 0.88);
+      padding-block: 24px;
+      padding: 24px;
+      padding-inline: 24px;
+      perspective-origin: 718.5px 66.8516px;
+      text-decoration: none solid rgba(0, 0, 0, 0.88);
+      text-emphasis-color: rgba(0, 0, 0, 0.88);
+      transform-origin: 718.5px 66.8555px;
+      width: 1437px;
+      -webkit-text-fill-color: rgba(0, 0, 0, 0.88);
+      -webkit-text-stroke-color: rgba(0, 0, 0, 0.88);
+    "
+  >
+    <h1
+      data-v-64ee0ba7=""
+      style="
+        block-size: 133.711px;
+        border-block-color: rgba(0, 0, 0, 0.88);
+        border-color: rgba(0, 0, 0, 0.88);
+        border-bottom-left-radius: 8px;
+        border-bottom-right-radius: 8px;
+        border-end-end-radius: 8px;
+        border-end-start-radius: 8px;
+        border-inline-color: rgba(0, 0, 0, 0.88);
+        caret-color: rgba(0, 0, 0, 0.88);
+        color: rgba(0, 0, 0, 0.88);
+        column-rule-color: rgba(0, 0, 0, 0.88);
+        font-family: -apple-system, 'system-ui', 'Segoe UI', Roboto,
+          'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
+          'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+        font-size: 14px;
+        height: 133.711px;
+        inline-size: 1437px;
+        line-height: 22px;
+        list-style-type: none;
+        outline-color: rgba(0, 0, 0, 0.88);
+        padding-block: 24px;
+        padding: 24px;
+        padding-inline: 24px;
+        perspective-origin: 718.5px 66.8516px;
+        text-decoration: none solid rgba(0, 0, 0, 0.88);
+        text-emphasis-color: rgba(0, 0, 0, 0.88);
+        transform-origin: 718.5px 66.8555px;
+        width: 1437px;
+        -webkit-text-fill-color: rgba(0, 0, 0, 0.88);
+        -webkit-text-stroke-color: rgba(0, 0, 0, 0.88);
+      "
+    >
+      早安，Serati Ma
+      <span
+        data-v-64ee0ba7=""
+        class="welcome-text"
+        style="
+          block-size: 133.711px;
+          border-block-color: rgba(0, 0, 0, 0.88);
+          border-color: rgba(0, 0, 0, 0.88);
+          border-bottom-left-radius: 8px;
+          border-bottom-right-radius: 8px;
+          border-end-end-radius: 8px;
+          border-end-start-radius: 8px;
+          border-inline-color: rgba(0, 0, 0, 0.88);
+          caret-color: rgba(0, 0, 0, 0.88);
+          color: rgba(0, 0, 0, 0.88);
+          column-rule-color: rgba(0, 0, 0, 0.88);
+          font-family: -apple-system, 'system-ui', 'Segoe UI', Roboto,
+            'Helvetica Neue', Arial, 'Noto Sans', sans-serif,
+            'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+            'Noto Color Emoji';
+          font-size: 14px;
+          height: 133.711px;
+          inline-size: 1437px;
+          line-height: 22px;
+          list-style-type: none;
+          outline-color: rgba(0, 0, 0, 0.88);
+          padding-block: 24px;
+          padding: 24px;
+          padding-inline: 24px;
+          perspective-origin: 718.5px 66.8516px;
+          text-decoration: none solid rgba(0, 0, 0, 0.88);
+          text-emphasis-color: rgba(0, 0, 0, 0.88);
+          transform-origin: 718.5px 66.8555px;
+          width: 1437px;
+          -webkit-text-fill-color: rgba(0, 0, 0, 0.88);
+          -webkit-text-stroke-color: rgba(0, 0, 0, 0.88);
+        "
+        >，祝你开心每一天！</span
+      >
+    </h1>
+    <p
+      data-v-64ee0ba7=""
+      class="desc"
+      style="
+        block-size: 133.711px;
+        border-block-color: rgba(0, 0, 0, 0.88);
+        border-color: rgba(0, 0, 0, 0.88);
+        border-bottom-left-radius: 8px;
+        border-bottom-right-radius: 8px;
+        border-end-end-radius: 8px;
+        border-end-start-radius: 8px;
+        border-inline-color: rgba(0, 0, 0, 0.88);
+        caret-color: rgba(0, 0, 0, 0.88);
+        color: rgba(0, 0, 0, 0.88);
+        column-rule-color: rgba(0, 0, 0, 0.88);
+        font-family: -apple-system, 'system-ui', 'Segoe UI', Roboto,
+          'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
+          'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+        font-size: 14px;
+        height: 133.711px;
+        inline-size: 1437px;
+        line-height: 22px;
+        list-style-type: none;
+        outline-color: rgba(0, 0, 0, 0.88);
+        padding-block: 24px;
+        padding: 24px;
+        padding-inline: 24px;
+        perspective-origin: 718.5px 66.8516px;
+        text-decoration: none solid rgba(0, 0, 0, 0.88);
+        text-emphasis-color: rgba(0, 0, 0, 0.88);
+        transform-origin: 718.5px 66.8555px;
+        width: 1437px;
+        -webkit-text-fill-color: rgba(0, 0, 0, 0.88);
+        -webkit-text-stroke-color: rgba(0, 0, 0, 0.88);
+      "
+    >
+      蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED
+    </p>
+  </div>
+</template>
+
+<style></style>
