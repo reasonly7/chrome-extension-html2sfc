563005b126f62d593fc0f94373f5a17dc792fec2
leishanglin <reasonly8@sina.com>
2024-12-09 18:09:20 +0800

    feat: add preview gif

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 Use GPT to convert HTML into a Single File Component (SFC).（用 GPT 将 HTML 转成单文件组件（SFC））
 
-1. 如果子元素的宽高等属性值和父元素的属性值相同，那子元素的属性值应当设为 100%
+See [./chrome-extension-7](./chrome-extension-7/)
 
+![](./preview.gif)
--- playground-ui/src/App.vue
+++ playground-ui/src/App.vue
@@ -2,22 +2,22 @@
 
 <template>
   <div class="button-container">
-    <a href="https://bailian.console.aliyun.com/" target="_blank" rel="nofollow" class="manage-console">管理控制台</a>
-    <a href="https://page.aliyun.com/form/act733831898/index.htm" target="_blank" class="consult-now">立即咨询</a>
+    <a href="https://bailian.console.aliyun.com/" target="_blank" rel="nofollow" class="manage-console-button">管理控制台</a>
+    <a href="https://page.aliyun.com/form/act733831898/index.htm" target="_blank" class="consult-button">立即咨询</a>
   </div>
 </template>
 
 <style lang="less" scoped>
 .button-container {
+  width: 336px;
   height: 44px;
   display: flex;
-  justify-content: center;
-  padding-top: 20px;
+  margin-top: 24px;
 
-  .manage-console {
+  .manage-console-button {
     background-color: rgb(255, 106, 0);
     border: 1px solid transparent;
-    color: rgb(255, 255, 255);
+    color: white;
     font-weight: 700;
     line-height: 42px;
     text-align: center;
@@ -27,7 +27,7 @@
     display: block;
   }
 
-  .consult-now {
+  .consult-button {
     border: 1px solid rgb(156, 164, 175);
     color: rgb(61, 73, 92);
     font-weight: 700;
