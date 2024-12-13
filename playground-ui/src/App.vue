9d1849803ca0910cdae6cd9e528b9fddc9e48082
leishanglin <reasonly8@sina.com>
2024-12-13 13:02:54 +0800

    feat: 添加 ManualIconButton 组件并移除 CardOne 组件
    - 在 `App.vue` 中引入并使用 `ManualIconButton` 组件。
    - 删除 `CardOne.vue` 组件及其相关样式和模板。
    - 更新 `App.vue` 的样式以适应新的组件布局。

--- playground-ui/src/App.vue
+++ playground-ui/src/App.vue
@@ -1,43 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ManualIconButton from "./components/ManualIconButton.vue";
+</script>
 
 <template>
-  <div class="button-container">
-    <a href="https://bailian.console.aliyun.com/" target="_blank" rel="nofollow" class="manage-console-button">管理控制台</a>
-    <a href="https://page.aliyun.com/form/act733831898/index.htm" target="_blank" class="consult-button">立即咨询</a>
+  <div class="outer-wrapper">
+    <ManualIconButton></ManualIconButton>
   </div>
 </template>
 
 <style lang="less" scoped>
-.button-container {
-  width: 336px;
-  height: 44px;
-  display: flex;
-  margin-top: 24px;
-
-  .manage-console-button {
-    background-color: rgb(255, 106, 0);
-    border: 1px solid transparent;
-    color: white;
-    font-weight: 700;
-    line-height: 42px;
-    text-align: center;
-    text-decoration: none;
-    width: 160px;
-    height: 44px;
-    display: block;
-  }
-
-  .consult-button {
-    border: 1px solid rgb(156, 164, 175);
-    color: rgb(61, 73, 92);
-    font-weight: 700;
-    line-height: 42px;
-    margin-left: 16px;
-    text-align: center;
-    text-decoration: none;
-    width: 160px;
-    height: 44px;
-    display: block;
-  }
+.outer-wrapper {
+  padding: 100px;
 }
 </style>
---
+++ playground-ui/src/components/ManualIconButton.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="manual-icon-button">
+    <img
+      src="https://img.alicdn.com/imgextra/i1/O1CN018Rdpz51IxGo11CYqq_!!6000000000959-55-tps-28-26.svg"
+    />
+    <span class="text">仓库通</span>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.manual-icon-button {
+  background-color: rgb(242, 244, 253);
+  width: 72px;
+  height: 72px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 16px;
+  padding: 7px 0;
+  cursor: pointer;
+  box-sizing: border-box;
+  justify-content: space-between;
+  align-items: center;
+  > img {
+    width: 32px;
+    height: 32px;
+    margin-bottom: 8px;
+  }
+  > .text {
+    font-size: 12px;
+    line-height: 18px;
+    color: #181c1f;
+  }
+  &:hover {
+    background-color: #fff;
+  }
+}
+</style>
